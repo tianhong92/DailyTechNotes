abf0341b8fa7765e768dd074bd5eb61a8e518730
tianhong229@gmail.com <tianhong229@gmail.com>
2019-01-14 17:32:17 +0800

    added changeable params

--- src/main/java/com/tianhong/demo/ChangeableParam.java
+++ src/main/java/com/tianhong/demo/ChangeableParam.java
@@ -5,4 +5,11 @@ package com.tianhong.demo;
  * @since 2019/1/14
  */
 public class ChangeableParam {
+    public static void main(String[] args) {
+        userLength(1, 2, "4");
+        userLength(new int[]{1, 2});
+    }
+    public static void userLength(Object... args){
+        System.out.println(args.length);
+    }
 }
