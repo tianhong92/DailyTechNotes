b316a666fc0c72f9672a0919fcb40b5b85174366
tianhong229@gmail.com <tianhong229@gmail.com>
2019-01-17 16:39:00 +0800

    basic thread

---
+++ src/main/java/com/tianhong/demo/RunnableDemo.java
@@ -0,0 +1,32 @@
+package com.tianhong.demo;
+
+class RunnableDemo implements Runnable {
+    private String threadName;
+    private Thread t;
+
+    RunnableDemo(String name){
+        this.threadName = name;
+    }
+
+    @Override
+    public void run() {
+        System.out.println("Running "+threadName);
+        for(int i = 4; i > 0; i--){
+            System.out.println(threadName + ", "+ i);
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+                System.out.println("Thread "+threadName+" interrupted.");
+            }
+        }
+        System.out.println(threadName+" exiting.");
+    }
+
+    public void start(){
+        System.out.println("Starting "+threadName);
+        if(t == null){
+            t = new Thread(this, threadName);
+            t.start();
+        }
+    }
+}
---
+++ src/main/java/com/tianhong/demo/ThreadDemo.java
@@ -0,0 +1,36 @@
+package com.tianhong.demo;
+
+/**
+ * @author TianhongWang
+ * @since 2019/1/17
+ */
+public class ThreadDemo extends Thread {
+    private Thread t;
+    private String threadName;
+
+    ThreadDemo(String name){
+        this.threadName = name;
+        System.out.println("Creating "+threadName);
+    }
+
+    public void run() {
+        System.out.println("Running "+threadName);
+        for(int i = 4; i > 0; i--){
+            System.out.println("Thread "+threadName+", "+i);
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+                System.out.println("Thread "+ threadName + " interrupted");
+            }
+        }
+        System.out.println("Thread "+threadName+" exiting.");
+    }
+
+    public void start(){
+        System.out.println("Starting " + threadName);
+        if(t == null){
+            t = new Thread(this, threadName);
+            t.start();
+        }
+    }
+}
