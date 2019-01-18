06322f50a0e501b4419cdcd876277e43c04ab328
tianhong229@gmail.com <tianhong229@gmail.com>
2019-01-18 14:24:49 +0800

    add demo

---
+++ src/main/java/com/tianhong/demo/thread/DisplayMessage.java
@@ -0,0 +1,19 @@
+package com.tianhong.demo.thread;
+
+/**
+ * @author TianhongWang
+ * @since 2019/1/17
+ */
+public class DisplayMessage implements Runnable {
+    private String message;
+
+    public DisplayMessage(String message) {
+        this.message = message;
+    }
+
+    public void run() {
+        while(true) {
+            System.out.println(message);
+        }
+    }
+}
